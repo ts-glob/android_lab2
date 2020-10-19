5ea96ec857097e9d166ce51fef9bed9594636f12
ts-glob <62998307+ts-glob@users.noreply.github.com>
2020-10-19 16:52:21 +0400

    Вторая лаборатораня работа Контакты по пин-коду

--- app/src/main/java/com/su/lab/LoginActivity.java
+++ app/src/main/java/com/su/lab/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.su.lab;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -73,6 +74,8 @@ public class LoginActivity extends Activity {
              *  TODO #2 Открыть MainActivity через Intent
              *   http://developer.alexanderklimov.ru/android/theory/intent.php
              */
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
         } else {
             Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show();
         }
